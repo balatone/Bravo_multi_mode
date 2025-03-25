6a19c59c97cd2e727f38eb39aa6de42717bd5740
Erik Alexander Christian Balaton <erik.balaton@sikt.no>
2025-03-25 17:58:40 +0100

    Added custom button support.

--- FlyWithLua/BravoMultiMode.lua
+++ FlyWithLua/BravoMultiMode.lua
@@ -18,13 +18,13 @@ local last_button_mode_state = false
 local last_button_mode_change_time = 0
 local last_button_cf_mode_state = false
 local last_button_cf_mode_change_time = 0
-local DEBOUNCE_DELAY = 0.2 -- 300 milliseconds debounce
+local DEBOUNCE_DELAY = 0.0 -- 300 milliseconds debounce
 
 -- Bindings for the selector knob
 local current_selection = "ALT"
 local selections1 = {"ALT","VS","HDG","CRS","IAS"}
-local selections2 = {"COM1","NAV1","BARO/CRS","RNG","FMS"}
-local selections3 = {"COM2","NAV2","BARO/CRS","RNG","FMS"}
+local selections2 = {"COM","NAV","BARO/CRS","RNG","FMS"}
+local selections3 = {"COM","NAV","BARO/CRS","RNG","FMS"}
 local SELECTOR1 = 660
 local SELECTOR2 = SELECTOR1 - 1
 local SELECTOR3 = SELECTOR1 - 2
@@ -43,17 +43,6 @@ local MODE_TOGGLE = 802
 -- Screen position for the overlay
 local OVERLAY_X = 50  -- X position (pixels from left)
 local OVERLAY_Y = SCREEN_HEIGHT - 500  -- Y position (pixels from top)
-
--- Create the custom window
-local window_id
-
-local message = ""
-
-
-lastClickX3 = 640 / 2
-lastClickY3 = 480 / 2
-
-local line_y_inc = 50
 
 -- imgui only works inside a floating window, so we need to create one first:
 my_floating_wnd = float_wnd_create(260, 80, 1, false)
@@ -81,7 +70,7 @@ function on_draw_floating_window(my_floating_wnd, x3, y3)
 	end
 	
     glColor3f(1, 1, 1) -- Balck semitransparent
-    draw_string_Helvetica_18(x3 + 80, v_offset + offset_selection, message)
+    draw_string_Helvetica_18(x3 + 80, v_offset + offset_selection, current_selection)
 
     local offset_mode = -20
 
@@ -121,7 +110,7 @@ function draw_mode_overlay()
 	end
 	
     glColor4f(1, 1, 1, 1) -- Balck semitransparent
-    draw_string_Helvetica_18(OVERLAY_X + 80, OVERLAY_Y + offset_selection, message)
+    draw_string_Helvetica_18(OVERLAY_X + 80, OVERLAY_Y + offset_selection, current_selection)
 
     local offset_mode = -20
 
@@ -181,52 +170,42 @@ function refresh_selector()
 	if button(SELECTOR1) then
         -- Action for Position 1
 		set_current_selector(1)
-        set_button_assignment(KNOB_INCREASE, "FlyWithLua/custom/knob_increase_handler_alt")
-		set_button_assignment(KNOB_DECREASE, "FlyWithLua/custom/knob_decrease_handler_alt")
     elseif button(SELECTOR2) then
         -- Action for Position 2
 		set_current_selector(2)
-        set_button_assignment(KNOB_INCREASE, "FlyWithLua/custom/knob_increase_handler_vs")
-		set_button_assignment(KNOB_DECREASE, "FlyWithLua/custom/knob_decrease_handler_vs")
     elseif button(SELECTOR3) then
         -- Action for Position 3
 		set_current_selector(3)
-        set_button_assignment(KNOB_INCREASE, "FlyWithLua/custom/knob_increase_handler_hdg")
-		set_button_assignment(KNOB_DECREASE, "FlyWithLua/custom/knob_decrease_handler_hdg")
     elseif button(SELECTOR4) then
         -- Action for Position 4
 		set_current_selector(4)
-        set_button_assignment(KNOB_INCREASE, "FlyWithLua/custom/knob_increase_handler_crs")
-		set_button_assignment(KNOB_DECREASE, "FlyWithLua/custom/knob_decrease_handler_crs")
     elseif button(SELECTOR5) then
         -- Action for Position 5
 		set_current_selector(5)
-        set_button_assignment(KNOB_INCREASE, "FlyWithLua/custom/knob_increase_handler_ias")
-		set_button_assignment(KNOB_DECREASE, "FlyWithLua/custom/knob_decrease_handler_ias")
     end
 end
 
 function set_current_selector(index)
 	if current_mode == "AUTO" then
-		message	= selections1[index]
+		current_selection	= selections1[index]
 	elseif current_mode == "PFD" then
-		message	= selections2[index]
+		current_selection	= selections2[index]
 	else
-		message	= selections3[index]
+		current_selection	= selections3[index]
 	end
 end
 
--- COM1
-local com1_outer_freq_up = "sim/GPS/g1000n1_com_outer_up"
-local com1_outer_freq_down = "sim/GPS/g1000n1_com_outer_down"
-local com1_inner_freq_up = "sim/GPS/g1000n1_com_inner_up"
-local com1_inner_freq_down = "sim/GPS/g1000n1_com_inner_down"
+-- COM1 PFD
+local com1_PFD_outer_freq_up = "sim/GPS/g1000n1_com_outer_up"
+local com1_PFD_outer_freq_down = "sim/GPS/g1000n1_com_outer_down"
+local com1_PFD_inner_freq_up = "sim/GPS/g1000n1_com_inner_up"
+local com1_PFD_inner_freq_down = "sim/GPS/g1000n1_com_inner_down"
 
--- COM2
-local com2_outer_freq_up = "sim/GPS/g1000n2_com_outer_up"
-local com2_outer_freq_down = "sim/GPS/g1000n2_com_outer_down"
-local com2_inner_freq_up = "sim/GPS/g1000n2_com_inner_up"
-local com2_inner_freq_down = "sim/GPS/g1000n2_com_inner_down"
+-- COM1 MFD
+local com1_MFD_outer_freq_up = "sim/GPS/g1000n3_com_outer_up"
+local com1_MFD_outer_freq_down = "sim/GPS/g1000n3_com_outer_down"
+local com1_MFD_inner_freq_up = "sim/GPS/g1000n3_com_inner_up"
+local com1_MFD_inner_freq_down = "sim/GPS/g1000n3_com_inner_down"
 
 -- Store original commands
 local original_altitude_up = "sim/autopilot/altitude_up"
@@ -239,15 +218,15 @@ function handle_bravo_knob_increase_alt()
         command_once(original_altitude_up) 
     elseif current_mode == "PFD" then
 	    if current_cf_mode == "outer" then
-			command_once(com1_outer_freq_up) 
+			command_once(com1_PFD_outer_freq_up) 
 		else
-			command_once(com1_inner_freq_up) 
+			command_once(com1_PFD_inner_freq_up) 
 		end
     elseif current_mode == "MFD" then
 	    if current_cf_mode == "outer" then
-			command_once(com2_outer_freq_up) 
+			command_once(com1_MFD_outer_freq_up) 
 		else
-			command_once(com2_inner_freq_up) 
+			command_once(com1_MFD_inner_freq_up) 
 		end
 	end
 end
@@ -266,15 +245,15 @@ function handle_bravo_knob_decrease_alt()
         command_once(original_altitude_down) 
     elseif current_mode == "PFD" then
 	    if current_cf_mode == "outer" then
-			command_once(com1_outer_freq_down) 
+			command_once(com1_PFD_outer_freq_down) 
 		else
-			command_once(com1_inner_freq_down) 
+			command_once(com1_PFD_inner_freq_down) 
 		end		
     elseif current_mode == "MFD" then
 	    if current_cf_mode == "outer" then
-			command_once(com2_outer_freq_down) 
+			command_once(com1_MFD_outer_freq_down) 
 		else
-			command_once(com2_inner_freq_down) 
+			command_once(com1_MFD_inner_freq_down) 
 		end		
     end
 end
@@ -288,17 +267,17 @@ create_command(
     ""
 )
 
--- NAV1
-local nav1_outer_freq_up = "sim/GPS/g1000n1_nav_outer_up"
-local nav1_outer_freq_down = "sim/GPS/g1000n1_nav_outer_down"
-local nav1_inner_freq_up = "sim/GPS/g1000n1_nav_inner_up"
-local nav1_inner_freq_down = "sim/GPS/g1000n1_nav_inner_down"
+-- NAV1 PFD
+local nav1_PFD_outer_freq_up = "sim/GPS/g1000n1_nav_outer_up"
+local nav1_PFD_outer_freq_down = "sim/GPS/g1000n1_nav_outer_down"
+local nav1_PFD_inner_freq_up = "sim/GPS/g1000n1_nav_inner_up"
+local nav1_PFD_inner_freq_down = "sim/GPS/g1000n1_nav_inner_down"
 
--- NAV2
-local nav2_outer_freq_up = "sim/GPS/g1000n2_nav_outer_up"
-local nav2_outer_freq_down = "sim/GPS/g1000n2_nav_outer_down"
-local nav2_inner_freq_up = "sim/GPS/g1000n2_nav_inner_up"
-local nav2_inner_freq_down = "sim/GPS/g1000n2_nav_inner_down"
+-- NAV1 MFD
+local nav1_MFD_outer_freq_up = "sim/GPS/g1000n3_nav_outer_up"
+local nav1_MFD_outer_freq_down = "sim/GPS/g1000n3_nav_outer_down"
+local nav1_MFD_inner_freq_up = "sim/GPS/g1000n3_nav_inner_up"
+local nav1_MFD_inner_freq_down = "sim/GPS/g1000n3_nav_inner_down"
 
 -- Store original commands
 local original_nose_up = "sim/autopilot/nose_up"
@@ -311,15 +290,15 @@ function handle_bravo_knob_increase_vs()
         command_once(original_nose_up) 
     elseif current_mode == "PFD" then
 	    if current_cf_mode == "outer" then
-			command_once(nav1_outer_freq_up) 
+			command_once(nav1_PFD_outer_freq_up) 
 		else
-			command_once(nav1_inner_freq_up) 
+			command_once(nav1_PFD_inner_freq_up) 
 		end
     elseif current_mode == "MFD" then
 	    if current_cf_mode == "outer" then
-			command_once(nav2_outer_freq_up) 
+			command_once(nav1_MFD_outer_freq_up) 
 		else
-			command_once(nav2_inner_freq_up) 
+			command_once(nav1_MFD_inner_freq_up) 
 		end
 	end
 end
@@ -338,15 +317,15 @@ function handle_bravo_knob_decrease_vs()
         command_once(original_nose_down) 
     elseif current_mode == "PFD" then
 	    if current_cf_mode == "outer" then
-			command_once(nav1_outer_freq_down) 
+			command_once(nav1_PFD_outer_freq_down) 
 		else
-			command_once(nav1_inner_freq_down) 
+			command_once(nav1_PFD_inner_freq_down) 
 		end		
     elseif current_mode == "MFD" then
 	    if current_cf_mode == "outer" then
-			command_once(nav2_outer_freq_down) 
+			command_once(nav1_MFD_outer_freq_down) 
 		else
-			command_once(nav2_inner_freq_down) 
+			command_once(nav1_MFD_inner_freq_down) 
 		end		
     end
 end
@@ -476,7 +455,7 @@ create_command(
     ""
 )
 
--- BARO and HSI
+-- FMS
 local fms1_outer_up = "sim/GPS/g1000n1_fms_outer_up"
 local fms1_inner_up = "sim/GPS/g1000n1_fms_inner_up"
 local fms1_outer_down = "sim/GPS/g1000n1_fms_outer_down"
@@ -519,6 +498,7 @@ create_command(
     "",
     ""
 )
+
 -- Function to handle button press based on mode
 function handle_bravo_knob_decrease_ias()
     if current_mode == "AUTO" then
@@ -543,6 +523,262 @@ create_command(
     "FlyWithLua/custom/knob_decrease_handler_ias",
     "Handle button on bravo that decrements based on mode and selection ias",
     "handle_bravo_knob_decrease_ias()", -- Call Lua function when pressed
+    "",
+    ""
+)
+
+-- IAS button
+
+-- Store original commands
+-- local original_ias_button = "sim/autopilot/autothrottle_toggle"
+local original_ias_button = "sim/autopilot/speed_hold"
+
+
+-- Function to handle button press based on mode
+function handle_bravo_ias_button()
+    if current_mode == "AUTO" then
+        command_once(original_ias_button) 
+    elseif current_mode == "PFD" then
+		if current_selection == "COM" then
+			cycle_cf_mode()
+		elseif current_selection == "NAV" then
+			cycle_cf_mode()
+		elseif current_selection == "BARO/CRS" then
+			cycle_cf_mode()
+		elseif current_selection == "FMS" then
+			cycle_cf_mode()
+		end
+    elseif current_mode == "MFD" then
+		if current_selection == "COM" then
+			cycle_cf_mode()
+		elseif current_selection == "NAV" then
+			cycle_cf_mode()
+		elseif current_selection == "BARO/CRS" then
+			cycle_cf_mode()
+		elseif current_selection == "FMS" then
+			cycle_cf_mode()
+		end			
+	end
+end
+
+-- Create a custom command for bravo knob increase
+create_command(
+    "FlyWithLua/custom/ias_button",
+    "Bravo++ toggles ias button",
+    "handle_bravo_ias_button()", -- Call Lua function when pressed
+    "",
+    ""
+)
+
+-- VS button
+local com1_PFD_vs_button = "sim/GPS/g1000n1_com_ff"
+local com1_MFD_vs_button = "sim/GPS/g1000n3_com_ff"
+local nav1_PFD_vs_button = "sim/GPS/g1000n1_nav_ff"
+local nav1_MFD_vs_button = "sim/GPS/g1000n3_nav_ff"
+local fms_PFD_vs_button = "sim/GPS/g1000n1_cursor"
+local fms_MFD_vs_button = "sim/GPS/g1000n3_cursor"
+
+
+-- Store original commands
+local original_vs_button = "sim/autopilot/vertical_speed"
+
+-- Function to handle button press based on mode
+function handle_bravo_vs_button()
+    if current_mode == "AUTO" then
+        command_once(original_vs_button) 
+    elseif current_mode == "PFD" then
+		if current_selection == "COM" then
+			command_once(com1_PFD_vs_button)
+		elseif current_selection == "NAV" then
+			command_once(nav1_PFD_vs_button)
+		elseif current_selection == "FMS" then
+			command_once(fms_PFD_vs_button) 
+		end
+    elseif current_mode == "MFD" then
+		if current_selection == "COM" then
+			command_once(com1_MFD_vs_button)
+		elseif current_selection == "NAV" then
+			command_once(nav1_MFD_vs_button)
+		elseif current_selection == "FMS" then
+			command_once(fms_MFD_vs_button)
+		end			
+	end
+end
+
+-- Create a custom command for bravo knob increase
+create_command(
+    "FlyWithLua/custom/vs_button",
+    "Bravo++ toggles vs button",
+    "handle_bravo_vs_button()", -- Call Lua function when pressed
+    "",
+    ""
+)
+
+-- ALT button
+local com_PFD_alt_button = "sim/GPS/g1000n1_com12"
+local com_MFD_alt_button = "sim/GPS/g1000n3_com12"
+local nav_PFD_alt_button = "sim/GPS/g1000n1_nav12"
+local nav_MFD_alt_button = "sim/GPS/g1000n3_nav12"
+
+local fms_PFD_alt_button = "sim/GPS/g1000n1_ent"
+local fms_MFD_alt_button = "sim/GPS/g1000n3_ent"
+
+-- Store original commands
+local original_alt_button = "sim/autopilot/altitude_hold"
+
+
+-- Function to handle button press based on mode
+function handle_bravo_alt_button()
+    if current_mode == "AUTO" then
+        command_once(original_alt_button) 
+    elseif current_mode == "PFD" then
+		if current_selection == "COM" then
+			command_once(com_PFD_alt_button) 		
+		elseif current_selection == "NAV" then
+			command_once(nav_PFD_alt_button) 
+		elseif current_selection == "FMS" then
+			command_once(fms_PFD_alt_button) 
+		end
+    elseif current_mode == "MFD" then
+		if current_selection == "COM" then
+			command_once(com_MFD_alt_button) 		
+		elseif current_selection == "NAV" then
+			command_once(nav_MFD_alt_button) 
+		elseif current_selection == "FMS" then
+			command_once(fms_MFD_alt_button)
+		end			
+	end
+end
+
+-- Create a custom command for bravo knob increase
+create_command(
+    "FlyWithLua/custom/alt_button",
+    "Bravo++ toggles alt button",
+    "handle_bravo_alt_button()", -- Call Lua function when pressed
+    "",
+    ""
+)
+
+-- REV button
+local fms_PFD_rev_button = "sim/GPS/g1000n1_clr"
+local fms_MFD_rev_button = "sim/GPS/g1000n3_clr"
+
+-- Store original commands
+local original_rev_button = "sim/autopilot/back_course"
+
+-- Function to handle button press based on mode
+function handle_bravo_rev_button()
+    if current_mode == "AUTO" then
+        command_once(original_rev_button) 
+    elseif current_mode == "PFD" then
+		if current_selection == "FMS" then
+			command_once(fms_PFD_rev_button) 
+		end
+    elseif current_mode == "MFD" then
+		if current_selection == "FMS" then
+			command_once(fms_MFD_rev_button)
+		end			
+	end
+end
+
+-- Create a custom command for bravo knob increase
+create_command(
+    "FlyWithLua/custom/rev_button",
+    "Bravo++ toggles rev button",
+    "handle_bravo_rev_button()", -- Call Lua function when pressed
+    "",
+    ""
+)
+
+-- APR button
+local fms_PFD_apr_button = "sim/GPS/g1000n1_proc"
+local fms_MFD_apr_button = "sim/GPS/g1000n3_proc"
+
+-- Store original commands
+local original_apr_button = "sim/autopilot/approach"
+
+-- Function to handle button press based on mode
+function handle_bravo_apr_button()
+    if current_mode == "AUTO" then
+        command_once(original_apr_button) 
+    elseif current_mode == "PFD" then
+		if current_selection == "FMS" then
+			command_once(fms_PFD_apr_button) 
+		end
+    elseif current_mode == "MFD" then
+		if current_selection == "FMS" then
+			command_once(fms_MFD_apr_button)
+		end			
+	end
+end
+
+-- Create a custom command for bravo knob increase
+create_command(
+    "FlyWithLua/custom/apr_button",
+    "Bravo++ toggles apr button",
+    "handle_bravo_apr_button()", -- Call Lua function when pressed
+    "",
+    ""
+)
+
+-- NAV button
+local fms_PFD_nav_button = "sim/GPS/g1000n1_fpl"
+local fms_MFD_nav_button = "sim/GPS/g1000n3_fpl"
+
+-- Store original commands
+local original_nav_button = "sim/GPS/g1000n3_nav"
+
+-- Function to handle button press based on mode
+function handle_bravo_nav_button()
+    if current_mode == "AUTO" then
+        command_once(original_nav_button)
+    elseif current_mode == "PFD" then
+		if current_selection == "FMS" then
+			command_once(fms_PFD_nav_button) 
+		end
+    elseif current_mode == "MFD" then
+		if current_selection == "FMS" then
+			command_once(fms_MFD_nav_button)
+		end			
+	end
+end
+
+-- Create a custom command for bravo knob increase
+create_command(
+    "FlyWithLua/custom/nav_button",
+    "Bravo++ toggles nav button",
+    "handle_bravo_nav_button()", -- Call Lua function when pressed
+    "",
+    ""
+)
+
+-- HDG button
+local fms_PFD_hdg_button = "sim/GPS/g1000n1_direct"
+local fms_MFD_hdg_button = "sim/GPS/g1000n3_direct"
+
+-- Store original commands
+local original_hdg_button = "sim/GPS/g1000n3_hdg"
+
+-- Function to handle button press based on mode
+function handle_bravo_hdg_button()
+    if current_mode == "AUTO" then
+        command_once(original_hdg_button)
+    elseif current_mode == "PFD" then
+		if current_selection == "FMS" then
+			command_once(fms_PFD_hdg_button) 
+		end
+    elseif current_mode == "MFD" then
+		if current_selection == "FMS" then
+			command_once(fms_MFD_hdg_button)
+		end			
+	end
+end
+
+-- Create a custom command for bravo knob increase
+create_command(
+    "FlyWithLua/custom/hdg_button",
+    "Bravo++ toggles hdg button",
+    "handle_bravo_hdg_button()", -- Call Lua function when pressed
     "",
     ""
 )
