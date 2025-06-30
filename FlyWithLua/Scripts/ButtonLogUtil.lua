282927211a5f8914523eb439aa408810339c5c4f
Erik Alexander Christian Balaton <erik.balaton@sikt.no>
2025-06-30 10:38:30 +0200

    Improved utility for determining the ALT selector button.

--- FlyWithLua/Scripts/ButtonLogUtil.lua
+++ FlyWithLua/Scripts/ButtonLogUtil.lua
@@ -18,13 +18,27 @@ function find_assigned_buttons()
     return active_buttons
 end
 
+local last_buttons_pressed = {}
+local last_button_pressed = 0
 function log_last_buttons_pressed()
-    buttons_pressed = find_assigned_buttons()
+    local buttons_pressed = find_assigned_buttons()
+
     for i = 1, #buttons_pressed do
-        if buttons_pressed[i] > 0 or buttons_pressed[i] < 1024 then
+        if table.find(last_buttons_pressed, buttons_pressed[i]) == nil and (buttons_pressed[i] > 0 and buttons_pressed[i] < 1024) then
             log.info("Last buttons pressed: " .. buttons_pressed[i])
+            last_button_pressed = buttons_pressed[i]
+            last_buttons_pressed = buttons_pressed
         end
     end
+    huge_bubble(MOUSE_X, MOUSE_Y, "ButtonLogUtils","Use this for determining the ALT selector button assigned to your Honeycomb Bravo by X-Plane.", "Just turn the left selector knob to ALT and note the number.", "Last button pressed: " .. last_button_pressed)
+end
+
+-- Helper function to find index in table (used for cycling modes)
+function table.find(t, value)
+    for i, v in ipairs(t) do
+        if v == value then return i end
+    end
+    return nil -- Not found.
 end
 
 if write_log then
